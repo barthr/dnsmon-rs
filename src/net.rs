02ead4fc3296fdb24e3f201d3b1363c7a3d4e0d7
barthr <info@bartfokker.com>
2024-05-09 15:26:48 +0200

    Move net functionality to seperate file

---
+++ src/net.rs
@@ -0,0 +1,19 @@
+use pnet::datalink;
+
+#[derive(Debug)]
+pub struct NetInterface {
+    pub index: u32,
+    pub name: String,
+}
+
+impl NetInterface {
+    pub fn from_name(name: &str) -> Option<Self> {
+        return datalink::interfaces()
+            .iter()
+            .find(|iface| iface.name == name)
+            .map(|iface| NetInterface {
+                index: iface.index,
+                name: iface.name.to_string(),
+            });
+    }
+}
